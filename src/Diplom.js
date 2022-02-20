7d6e1f52cee5c7cd388ea4ae1ab155e3138d88b9
denis <denismatysik@gmail.com>
2022-02-20 22:57:55 +0300

    fix

--- src/Diplom.js
+++ src/Diplom.js
@@ -1,27 +1,11 @@
 import React from 'react'
-import "./MainStyle.css"
-import Title from './components/title/Title'
-import Header from './components/header/Header'
-import Content from './components/content/Content'
-import { Provider } from 'react-redux'
-import { store } from './components/redux/store'
-import { Toaster } from 'react-hot-toast'
-import Validation from './components/validation/Validation'
+import Main from './pages/Main'
 
 
 export default function Diplom() {
   return (
-    <Provider store={store}>
-      <div className="container">
-      <Title>Todo List</Title>
-      <Header>
-      </Header>
-      <Content></Content>
-      <Toaster
-        position="bottom-left"
-        reverseOrder={false}/>
-    </div>
-      <Validation/>
-    </Provider>
+   <div>
+     <Main/>
+   </div>
   )
 }
---
+++ src/pages/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../MainStyle.css"
+import Header from '../components/header/Header'
+import Content from '../components/content/Content'
+import { Provider } from 'react-redux'
+import { store } from '../components/redux/store'
+import { Toaster } from 'react-hot-toast'
+import Validation from '../components/validation/Validation'
+import Title from '../components/title/Title'
+
+export default function Main() {
+  return (
+    <Provider store={store}>
+      <div className="container">
+      <Title>Todo List</Title>
+      <Header>
+      </Header>
+      <Content></Content>
+      <Toaster
+        position="bottom-left"
+        reverseOrder={false}/>
+    </div>
+      <Validation/>
+    </Provider>
+  )
+}
