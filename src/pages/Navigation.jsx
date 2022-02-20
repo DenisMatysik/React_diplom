efe69877c1d39559f53e7b1f42f844136a5ad841
denis <denismatysik@gmail.com>
2022-02-21 00:28:07 +0300

    fix

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -8,15 +8,23 @@ const navStyle = {
   justifyContent: "space-between",
   height: "60px",
   maxWidth: "1140px",
-  margin: "0 auto"
+  margin: "0 auto",
+}
+
+const navContainer = {
+  width: "100%",
+  backgroundColor: "#b3b3b3",
 }
 
 export default function Navigation () {
   return (
-    <div style={navStyle} className='navigation'>
+    <div style={navContainer}>
+      <div style={navStyle}>
      <Button variant="outline-dark">Main page</Button>
      <Button variant="outline-dark">News page</Button>
      <Button variant="outline-dark">Login</Button>
     </div>
+  </div>
+    
   )
 }
