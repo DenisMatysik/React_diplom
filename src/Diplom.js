3eaea0fd86e3fd49d6d724e426a54139e87a450b
denis <denismatysik@gmail.com>
2022-02-20 22:50:35 +0300

    diplom

--- src/Diplom.js
+++ src/Diplom.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import "./diplom/MainStyle.css"
-import Title from './diplom/components/title/Title'
-import Header from './diplom/components/header/Header'
-import Content from './diplom/components/content/Content'
+import "./MainStyle.css"
+import Title from './components/title/Title'
+import Header from './components/header/Header'
+import Content from './components/content/Content'
 import { Provider } from 'react-redux'
-import { store } from './diplom/components/redux/store'
+import { store } from './components/redux/store'
 import { Toaster } from 'react-hot-toast'
-import Validation from './diplom/components/validation/Validation'
+import Validation from './components/validation/Validation'
 
 
 export default function Diplom() {
