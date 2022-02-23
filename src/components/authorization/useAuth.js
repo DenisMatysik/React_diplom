31c3e56cc8b915cb96725e768c7ba65338f534ba
denis <denismatysik@gmail.com>
2022-02-23 17:00:18 +0300

    fix

--- src/components/authorization/useAuth.js
+++ src/components/authorization/useAuth.js
@@ -4,3 +4,5 @@ import { AuthContext } from "./AuthProvider";
 export function useAuth(){
     return useContext(AuthContext);
 }
+
+// вызов useAuth позволит получать данные к обьекту {user, singin, singout}, т.е. сможем потом достать user
